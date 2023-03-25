a90fd362b9650fe1e32a8ab19b06b7aa7b841207
danieldz21 <daniel.da.silva.tj2011@gmail.com>
2023-03-25 15:49:41 -0300

    Finish Order api endpoint

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: :show
+
   def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      @order
+    else
+      render json: @order.errors, status: :unprocessable_entity
+    end
   end
 
   def show
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :phone_number, :city, :neighborhood,:street, :number, :complement,
+                                  :restaurant_id, order_products_attributes: [:product_id, :quantity])
+  end
+
+  def set_order
+    @order = Order.find(params[:id])
   end
 end
